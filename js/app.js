6ea2e09b7aa4774bada8e23f0ca04d672b73f010
atomoskiddevil <pathomphong.i@ku.th>
2018-02-23 01:39:19 +0700

    Set law woi 1

--- js/app.js
+++ js/app.js
@@ -1,54 +1,62 @@
 function rightTriangle(side1,side2)
 {
-    var a = side1*side1
-    var b = side2*side2
-    var result = Math.sqrt(a+b)
-    result = Math.floor(result)
-    return result
+    var a = side1*side1;
+    var b = side2*side2;
+    var result = Math.sqrt(a+b);
+    result = Math.floor(result);
+    return result;
 }
 
 function isoscelesTriangle(side1,side2)
 {
     if(side1>side2)
     {
-        result = side1
+        result = side1;
     }
     else if(side1 == side2)
     {
-        result = 1
+        result = 1;
     }
     else
     {
-        result = side2
+        result = side2;
     }
-    return result
+    return result;
 }
 
 function equilateralTriangle(side1,side2)
 {
     if(side1 == side2)
     {
-        result = side1
+        result = side1;
 
     }
     else
     {
-        result = "ไม่สามารถคำนวนได้"
+        result = "ไม่สามารถคำนวนได้";
     }
-    return result
+    return result;
 
 }
 
 function scaleneTriangle(side1,side2)
 {
+    console.log("GGWP");
     if(side1 == side2)
     {
-        result = "ไม่สามารถคำนวนได้"
+        result = "ไม่สามารถคำนวนได้";
     }
     else
     {
-        result = (side1+side2)-1
+        a = parseInt(side1);
+        b = parseInt(side2);
+        result = (a+b)-1;
+        if (result == side1 || result == side2)
+        {
+            result = "ไม่สามารถคำนวนได้";
+        }
+
 
     }
-    return result
+    return result;
 }
