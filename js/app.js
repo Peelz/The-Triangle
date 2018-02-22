b5552b412f730cbd5dc2a6e91a2e258906ac750f
atomoskiddevil <pathomphong.i@ku.th>
2018-02-23 00:58:52 +0700

    add call fn

--- js/app.js
+++ js/app.js
@@ -1,54 +1,54 @@
- function rightTriangle(side1,side2)
-        {
-            var a = side1*side1
-            var b = side2*side2
-            var result = Math.sqrt(a+b)
+function rightTriangle(side1,side2)
+{
+    var a = side1*side1
+    var b = side2*side2
+    var result = Math.sqrt(a+b)
 
-            return result
-        }
-        
-        function isoscelesTriangle(side1,side2)
-        {
-            if(side1>side2)
-            {
-                var result = side1
-            }
-            else if(side1 == side2)
-            {
-                var result = 1
-            }
-            else
-            {
-                var result = side2
-            }
-            return result
-        }
-        
-        function equilateralTriangle(side1,side2)
-        {
-            if(side1 == side2)
-            {
-                var result = side1
+    return result
+}
 
-            }
-            else
-            {
-                var result = x
-            }
-            return result
+function isoscelesTriangle(side1,side2)
+{
+    if(side1>side2)
+    {
+        result = side1
+    }
+    else if(side1 == side2)
+    {
+        result = 1
+    }
+    else
+    {
+        result = side2
+    }
+    return result
+}
 
-        }
-        
-        function scaleneTriangle(side1,side2)
-        {
-            if(side1 == side2)
-            {
-                var result = x
-            }
-            else
-            {
-                var result = (side1+side2)-1
+function equilateralTriangle(side1,side2)
+{
+    if(side1 == side2)
+    {
+        result = side1
 
-            }
-            return result
-        }
+    }
+    else
+    {
+        result = ""
+    }
+    return result
+
+}
+
+function scaleneTriangle(side1,side2)
+{
+    if(side1 == side2)
+    {
+        result = ""
+    }
+    else
+    {
+        result = (side1+side2)-1
+
+    }
+    return result
+}
